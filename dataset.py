dcb0553d2ef8c9648f2ef592c5efac5a0c02ab8f
Edgar Sebastian Almagro Ortiz <sbalmagro@gmail.com>
2021-02-28 13:02:20 +0100

    Replace get item functions.

--- dataset.py
+++ dataset.py
@@ -10,10 +10,11 @@ from torch.utils.data import Dataset
 
 
 class DirDataset(Dataset):
-    def __init__(self, img_dir, mask_dir, scale=1):
+    def __init__(self, img_dir, mask_dir, scale=1,pytorch=True):
         self.img_dir = img_dir
         self.mask_dir = mask_dir
         self.scale = scale
+        self.pytorch = pytorch
 
         try:
             self.ids = [s.split('.')[0] for s in os.listdir(self.img_dir)]
@@ -35,7 +36,7 @@ class DirDataset(Dataset):
         _img = np.array(_img)
         if len(_img.shape) == 2:  ## gray/mask images
             _img = np.expand_dims(_img, axis=-1)
-            
+
         # hwc to chw
         _img = _img.transpose((2, 0, 1))
         if _img.max() > 1:
@@ -45,22 +46,55 @@ class DirDataset(Dataset):
                 _img = _img / 255.
         return _img
 
-    def __getitem__(self, i):
-        idx = self.ids[i]
-        img_files = glob.glob(os.path.join(self.img_dir, idx+'.*'))
-        mask_files = glob.glob(os.path.join(self.mask_dir, idx+'.*'))
+    def open_as_array(self, _img, invert=False):
 
+        raw_rgb = np.array(_img.resize((256,256)))
+
+        if invert:
+            raw_rgb = raw_rgb.transpose((2,0,1))
+
+        # normalize
+        return (raw_rgb / np.iinfo(raw_rgb.dtype).max)
+
+    def open_mask(self, _mask, add_dims=False):
+
+        raw_mask = np.array(_mask.getchannel(0).resize((256,256)))
+
+        # plt.figure()
+        # image = tf_2_tensor(Image.open(self.files[idx]['gt']))
+        # inp = np.array(tf_2_PIL(image))
+        # plt.imshow(inp)
+        # print("inp",inp.shape)
+
+        raw_mask = np.where(raw_mask==255, 1, 0)
+
+        return np.expand_dims(raw_mask, 0) if add_dims else raw_mask
+
+    def __getitem__(self, i):
+
+        idx = self.ids[i]
+        img_files = glob.glob(os.path.join(self.img_dir, idx + '.*'))
+        mask_files = glob.glob(os.path.join(self.mask_dir, idx + '.*'))
         assert len(img_files) == 1, f'{idx}: {img_files}'
         assert len(mask_files) == 1, f'{idx}: {mask_files}'
 
         # use Pillow's Image to read .gif mask
         # https://answers.opencv.org/question/185929/how-to-read-gif-in-python/
+
         img = Image.open(img_files[0])
         mask = Image.open(mask_files[0])
         assert img.size == mask.size, f'{img.shape} # {mask.shape}'
 
-        img = self.preprocess(img, "image")
-        mask = self.preprocess(ImageOps.grayscale(mask), "mask")
+        xx = torch.tensor(self.open_as_array(img, invert=self.pytorch),
+                          dtype=torch.float32)
+        yy = torch.tensor(self.open_mask(mask, add_dims=False),
+                          dtype=torch.torch.int64)
 
-        return  torch.from_numpy(img).float(), \
-            torch.from_numpy(mask).float()
+        return xx, yy
+
+        # img = self.preprocess(img, "image")
+        # mask = self.preprocess(ImageOps.grayscale(mask), "mask")
+        # x = torch.from_numpy(img).float()
+        # y = torch.from_numpy(mask).float()
+
+        # return x,y
